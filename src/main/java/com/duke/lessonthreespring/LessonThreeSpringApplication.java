124e516c611cd906c01ff25613edb9359326da0f
duke <dukekunyura@gmail.ru>
2023-02-28 23:30:16 +0300

    first commit

---
+++ src/main/java/com/duke/lessonthreespring/LessonThreeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.duke.lessonthreespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonThreeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonThreeSpringApplication.class, args);
+	}
+
+}
